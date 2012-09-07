a09f0d4f08887d962c832fd0fef8bab693468129
Frank Spijkerman <frank.spijkerman@avira.com>
2012-09-07 09:56:45 +0000

    Ferdorified ensureaugeas

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,8 +1,9 @@
 class ensureaugeas {
-    package { [ "augeas-lenses",
-        "augeas-tools",
-        "libaugeas0",
-        "libaugeas-ruby1.8" ]:
+    $augeas_packages = $operatingsystem ? {
+        Ubuntu => [ "augeas-lenses", "augeas-tools", "libaugeas0", "libaugeas-ruby1.8" ],
+        Fedora => [ "augeas", "augeas-libs", "ruby-augeas" ]
+    } 
+    package { $augeas_packages:
             ensure => "present"
     }
 }
